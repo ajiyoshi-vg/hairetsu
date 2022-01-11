48309ca3532719e469ac267821e109413aa7892a
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2022-01-11 09:58:55 +0900

    add byetrie file

---
+++ bytetrie.go
@@ -0,0 +1,39 @@
+package hairetsu
+
+import (
+	da "github.com/ajiyoshi-vg/hairetsu/doublearray"
+	"github.com/ajiyoshi-vg/hairetsu/keyset"
+	"github.com/ajiyoshi-vg/hairetsu/node"
+	"github.com/ajiyoshi-vg/hairetsu/word"
+)
+
+type ByteTrie struct {
+	dat *da.DoubleArray
+}
+
+type ByteTrieBuilder struct {
+	builder *da.Builder
+}
+
+func newBuilder() *ByteTrieBuilder {
+	return &ByteTrieBuilder{
+		builder: da.NewBuilder(),
+	}
+}
+
+func (b *ByteTrieBuilder) FromBytes(xs [][]byte) (*ByteTrie, error) {
+	ret := da.New(len(xs) * 2)
+	ks := keyset.FromBytes(xs)
+	if err := b.builder.Build(ret, ks); err != nil {
+		return nil, err
+	}
+	return &ByteTrie{dat: ret}, nil
+}
+
+func (t *ByteTrie) ExactMatchSearch(key []byte) (node.Index, error) {
+	return t.dat.ExactMatchSearch(word.FromBytes(key))
+}
+
+func (t *ByteTrie) CommonPrefixSearch(key []byte) ([]node.Index, error) {
+	return t.dat.CommonPrefixSearch(word.FromBytes(key))
+}
