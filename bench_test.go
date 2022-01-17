a808099c445c6f8ead043e8aeea99d4df466db80
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2022-01-17 10:34:36 +0900

    BinaryUnmarshaler

--- bench_test.go
+++ bench_test.go
@@ -213,7 +213,7 @@ func readDict(path string) (runedict.RuneDict, error) {
 		return nil, err
 	}
 	ret := runedict.RuneDict{}
-	if err := ret.UnmarshalText(string(buf)); err != nil {
+	if err := ret.UnmarshalText(buf); err != nil {
 		return nil, err
 	}
 	return ret, nil
