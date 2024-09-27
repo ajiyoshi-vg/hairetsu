e046d84fcd0074660bdcfd2c933b9060abc747e3
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2024-09-27 18:21:52 +0900

    box test

--- codec/doublebyte/codec_test.go
+++ codec/doublebyte/codec_test.go
@@ -9,55 +9,40 @@ import (
 
 func TestDoubleByte(t *testing.T) {
 	cases := map[string]struct {
-		dict   WordDict
 		input  []byte
 		expect []byte
 	}{
-		"id:0": {
-			dict:   &codec.Identity[uint16]{},
+		"0": {
 			input:  []byte{0},
 			expect: []byte{0},
 		},
-		"id:0,1": {
-			dict:   &codec.Identity[uint16]{},
+		"0,1": {
 			input:  []byte{0, 1},
 			expect: []byte{0, 1},
 		},
-		"id:0,1,2": {
-			dict:   &codec.Identity[uint16]{},
+		"0,1,2": {
 			input:  []byte{0, 1, 2},
 			expect: []byte{0, 1, 2},
-		},
-		"map:1,2": {
-			dict:   codec.MapDict[uint16]{},
-			input:  []byte{1, 2},
-			expect: []byte{1, 2},
-		},
-		"map:1,2,3": {
-			dict:   codec.MapDict[uint16]{},
-			input:  []byte{1, 2, 3},
-			expect: []byte{1, 2, 3},
-		},
-		"array:1,2": {
-			dict:   codec.NewArrayDict[uint16](),
-			input:  []byte{1, 2},
-			expect: []byte{1, 2},
-		},
-		"array:1,2,3": {
-			dict:   codec.NewArrayDict[uint16](),
-			input:  []byte{1, 2, 3},
-			expect: []byte{1, 2, 3},
 		},
 	}
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			dict := codec.InstantCount(c.dict, DoubleBytes(c.input))
-			enc := NewEncoder(dict)
-			dec := enc.Decoder()
-			actual, err := dec.Decode(enc.Encode(c.input))
-			assert.NoError(t, err)
-			assert.Equal(t, c.input, actual)
+			kinds := map[string]WordDict{
+				"map":   codec.MapDict[uint16]{},
+				"array": codec.NewArrayDict[uint16](),
+				"id":    &codec.Identity[uint16]{},
+			}
+			for kind, d := range kinds {
+				t.Run(kind, func(t *testing.T) {
+					dict := codec.InstantCount(d, DoubleBytes(c.input))
+					enc := NewEncoder(dict)
+					dec := enc.Decoder()
+					actual, err := dec.Decode(enc.Encode(c.input))
+					assert.NoError(t, err)
+					assert.Equal(t, c.expect, actual)
+				})
+			}
 		})
 	}
 }
