f14fb041041aa567008e454d3b185d97da002e2c
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2024-09-27 17:28:23 +0900

    counter

--- codec/doublebyte/build.go
+++ codec/doublebyte/build.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	"github.com/ajiyoshi-vg/external/scan"
+	"github.com/ajiyoshi-vg/hairetsu/codec"
 	"github.com/ajiyoshi-vg/hairetsu/doublearray/item"
 )
 
@@ -12,11 +13,11 @@ type Factory interface {
 }
 
 func FromReadSeeker[T FillableDict](r io.ReadSeeker, f Factory, dict T) error {
-	b := newBuilder(dict)
+	b := codec.NewCounter(dict)
 	for line := range scan.ByteLines(r) {
-		b.add(line)
+		b.Add(DoubleBytes(line))
 	}
-	b.build()
+	b.Build()
 
 	if _, err := r.Seek(0, io.SeekStart); err != nil {
 		return err
@@ -31,31 +32,8 @@ func FromReadSeeker[T FillableDict](r io.ReadSeeker, f Factory, dict T) error {
 	return nil
 }
 
-type builder[T Fillable] struct {
-	count map[uint16]int
-	dest  T
-}
-
-func newBuilder[T Fillable](dest T) *builder[T] {
-	return &builder[T]{
-		count: make(map[uint16]int),
-		dest:  dest,
-	}
-}
-
-func (b *builder[T]) add(x []byte) {
-	for i := range DoubleBytes(x) {
-		b.count[i] += 1
-	}
-}
-
-func (b *builder[T]) build() T {
-	b.dest.Fill(b.count)
-	return b.dest
-}
-
 func instantBuild[T Fillable](dest T, data []byte) T {
-	b := newBuilder(dest)
-	b.add(data)
-	return b.build()
+	b := codec.NewCounter(dest)
+	b.Add(DoubleBytes(data))
+	return b.Build()
 }
